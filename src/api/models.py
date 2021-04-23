e47f793f6a382737e0481185f086af69cd894769
Arult <52519513+universuen@users.noreply.github.com>
2021-04-23 10:02:46 +0800

    fixed bugs

--- src/api/models.py
+++ src/api/models.py
@@ -4,12 +4,9 @@ from pydantic import BaseModel
 class Topic(BaseModel):
     name: str
     abstract: str
-    year: int
-    quarter: int
     heat: int
     pnum: int
     pic_url: str
-    source: str
 
 
 class Passage(BaseModel):
@@ -18,7 +15,6 @@ class Passage(BaseModel):
     website: str
     date: str
     url: str
-    topic: str
 
 
 if __name__ == '__main__':
